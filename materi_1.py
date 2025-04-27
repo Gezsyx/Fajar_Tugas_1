19f11169070d95334ff11275cb44f39e63df946d
Gezsyx <fajarabdulaziz259@gmail.com>
2025-04-27 10:24:20 +0700

    first commit

---
+++ materi_1.py
@@ -0,0 +1,17 @@
+variabel_1='Nama:Auliya Rahmah'
+variabel_2='Umur:19 Tahun'
+Variabel_3='Minuman Favorit:Kopi'
+
+print(variabel_1)
+print(variabel_2)
+print(Variabel_3)
+
+tipe_data_list = ['Auliya', 'Rahmah', 19, 'Kopi']
+print(tipe_data_list)
+
+data_dictionary = {
+    'Nama': 'Auliya Rahmah',
+    'Umur': 19,
+    'Minuman Favorit': 'Kopi'
+}
+print(data_dictionary)
